551dc6f0c7013171b57f3a21cbc3bb5a98505d42
csabourdin <kanedafromparis@gmail.com>
2015-10-07 20:01:14 +0200

    sol-00300

--- check.sh
+++ check.sh
@@ -3,4 +3,6 @@
 # This is a verification script
 # Ii should out put 1
 #
-TMPVAR=$(ssh nodexwiki java -version 2>&1) && echo "$TMPVAR" | grep "25.60-b23" | wc -l
+TMPVAR=$(ssh nodexwiki java -version 2>&1) && echo "$TMPVAR" | grep "24.79-b02" | wc -l
+TMPVAR=$(ssh nodexwiki mysql -u xwiki --version 2>&1) && echo "$TMPVAR" | grep "5.5.44" | wc -l
+curl -s http://nodexwiki:8080/ | grep "tomcat7-admin" | wc -l
