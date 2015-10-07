a655473daf1718ccc94f14d43392a54345630a36
csabourdin <kanedafromparis@gmail.com>
2015-10-07 19:32:05 +0200

    sol-00110

--- check.sh
+++ check.sh
@@ -3,4 +3,4 @@
 # This is a verification script
 # Ii should out put 1
 #
-TMPVAR=$(ssh nodexwiki java -version 2>&1) && echo "$TMPVAR" | grep "24.79-b02" | wc -l
+TMPVAR=$(ssh nodexwiki java -version 2>&1) && echo "$TMPVAR" | grep "25.60-b23" | wc -l
